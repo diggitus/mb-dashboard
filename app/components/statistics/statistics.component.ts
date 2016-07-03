22e699c913e657c007135a99ef834235a319db8d
Adam Just <info@adamjust.de>
2016-07-03 19:55:38 +0200

    Animate statistics page

--- app/components/statistics/statistics.component.ts
+++ app/components/statistics/statistics.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/core';
 
 
 @Component({
   selector: 'statistics',
   templateUrl: 'app/components/statistics/statistics.component.html',
-  styleUrls: ['app/components/statistics/statistics.component.css']
+  styleUrls: ['app/components/statistics/statistics.component.css'],
+  animations: [
+      trigger('flyInOut', [
+        state('in', style({transform: 'translateX(0)'})),
+        transition('void => *', [
+          style({transform: 'translateX(-100%)'}),
+          animate(1000)
+        ]),
+        transition('* => void', [
+          animate(1000, style({transform: 'translateX(100%)'}))
+        ])
+      ])
+  ]
 })
 export class StatisticsComponent implements OnInit {
 
