3592eebe5cfe4e42e139400a207fe3be16e1ef52
Adam Just <info@adamjust.de>
2016-07-06 19:51:11 +0200

    clean up

--- app/components/dashboard/mock-assets.ts
+++ app/components/dashboard/mock-assets.ts
@@ -3,87 +3,87 @@ import { Asset } from './asset';
 export let ASSETS: Asset[] = [
     {
         "id": 1,
-        "path": "images/a45-amg.jpg",
+        "path": "../../app/resources/images/a45-amg.jpg",
         "width": 1024,
         "height": 682
     }, {
         "id": 2,
-        "path": "images/c120.jpg",
+        "path": "../../app/resources/images/c120.jpg",
         "width": 1230,
         "height": 582
     }, {
         "id": 3,
-        "path": "images/c232.jpg",
+        "path": "../../app/resources/images/c232.jpg",
         "width": 800,
         "height": 530
     }, {
         "id": 4,
-        "path": "images/daimler_1886.jpg",
+        "path": "../../app/resources/images/daimler_1886.jpg",
         "width": 800,
         "height": 530
     }, {
         "id": 5,
-        "path": "images/daimler_cla_mercedes.jpg",
+        "path": "../../app/resources/images/daimler_cla_mercedes.jpg",
         "width": 568,
         "height": 237
     }, {
         "id": 6,
-        "path": "images/daimler_star.jpg",
+        "path": "../../app/resources/images/daimler_star.jpg",
         "width": 450,
         "height": 300
     }, {
         "id": 7,
-        "path": "images/F-015-Luxury.jpg",
+        "path": "../../app/resources/images/F-015-Luxury.jpg",
         "width": 1180,
         "height": 559
     }, {
         "id": 8,
-        "path": "images/mercedes-amg-gt-5.jpg",
+        "path": "../../app/resources/images/mercedes-amg-gt-5.jpg",
         "width": 900,
         "height": 600
     }, {
         "id": 9,
-        "path": "images/Mercedes-Benz_GLK-Klasse.jpg",
+        "path": "../../app/resources/images/Mercedes-Benz_GLK-Klasse.jpg",
         "width": 799,
         "height": 532
     }, {
         "id": 10,
-        "path": "images/mercedes-benz-a-class-w176.jpg",
+        "path": "../../app/resources/images/mercedes-benz-a-class-w176.jpg",
         "width": 814,
         "height": 383
     }, {
         "id": 11,
-        "path": "images/mercedes-benz-cla-class-117.jpg",
+        "path": "../../app/resources/images/mercedes-benz-cla-class-117.jpg",
         "width": 740,
         "height": 295
     }, {
         "id": 12,
-        "path": "images/mercedes-benz-sf1.jpg",
+        "path": "../../app/resources/images/mercedes-benz-sf1.jpg",
         "width": 620,
         "height": 361
     }, {
         "id": 13,
-        "path": "images/mercedes-cls.jpg",
+        "path": "../../app/resources/images/mercedes-cls.jpg",
         "width": 610,
         "height": 343
     }, {
         "id": 14,
-        "path": "images/slk-200.jpg",
+        "path": "../../app/resources/images/slk-200.jpg",
         "width": 630,
         "height": 420
     }, {
         "id": 15,
-        "path": "images/van.jpg",
+        "path": "../../app/resources/images/van.jpg",
         "width": 960,
         "height": 298
     }, {
         "id": 16,
-        "path": "images/w180.jpg",
+        "path": "../../app/resources/images/w180.jpg",
         "width": 1000,
         "height": 563
     }, {
         "id": 17,
-        "path": "images/z100.jpg",
+        "path": "../../app/resources/images/z100.jpg",
         "width": 510,
         "height": 335
     }
