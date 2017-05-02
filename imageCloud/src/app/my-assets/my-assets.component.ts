ea0f80b8cda6fa9306dc36cfbc483fc196a66ce3
Adam Just <info@adamjust.de>
2017-05-02 18:19:03 +0200

    Testing table and flex layout

--- imageCloud/src/app/my-assets/my-assets.component.ts
+++ imageCloud/src/app/my-assets/my-assets.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// see https://www.iandevlin.com/blog/2013/06/css/css-stacking-with-display-table
+
 @Component({
   selector: 'app-my-assets',
   templateUrl: './my-assets.component.html',
